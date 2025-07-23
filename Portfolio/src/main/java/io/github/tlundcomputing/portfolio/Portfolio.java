5bd4034bf95537280878923c13113e050544c328
TLundComputing <33316738+TLundComputing@users.noreply.github.com>
2025-07-23 10:07:33 +0100

    Updating Licence and skeleton project

---
+++ Portfolio/src/main/java/io/github/tlundcomputing/portfolio/Portfolio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.github.tlundcomputing.portfolio;
+
+/**
+ *
+ * @author WINDOWS 11 USER
+ */
+public class Portfolio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
